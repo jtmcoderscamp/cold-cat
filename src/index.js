890e29914cbd39870c28fedfc77bbd3fdc4eea31
Teresa Skrzypczyk <tereska.skrzypczyk@gmail.com>
2019-11-19 20:54:27 +0100

    get API - basic version

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@
  */
 
 console.log("...");
+
+function getweather(cityName) {
+    var apiKey = '84af29f03ef35bb4dd7f1e9b0ad575e2';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&lang=pl&appid=' + apiKey)  
+    .then(function(resp) { 
+        return resp.json() })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+  }
+  
+getweather('Wroclaw');
