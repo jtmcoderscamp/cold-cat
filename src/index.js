9e7349bfbc1e519c3be7b4924567cd0c3042da29
Teresa Ziółkowska <terea.m.ziolkowska@gmail.com>
2019-11-21 15:57:43 +0100

    API with constructor and basic html

--- src/index.js
+++ src/index.js
@@ -3,4 +3,40 @@
  * Code written here will be executed when application launches
  */
 
-console.log("...");
+class WeatherDataSource {
+    constructor() {
+        this.apiKey = '84af29f03ef35bb4dd7f1e9b0ad575e2';
+    }  
+    getweather(city) {
+      
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=pl&appid=' + this.apiKey)  
+        .then(function(resp) { 
+            return resp.json() })
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    }
+
+}
+
+const input = document.getElementById("submit");
+var city = "";
+var Weather = new WeatherDataSource();
+
+input.addEventListener('click', function(e) {
+    e.preventDefault();
+    city = document.querySelector("#city").value;
+    Weather.getweather(city);
+});
+
+
+
+
+
+    
+
+
+
