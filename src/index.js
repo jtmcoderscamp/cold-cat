fb1b72dc9ec3e680503ac80f8ae72ec8f682ff42
Teresa Ziółkowska <terea.m.ziolkowska@gmail.com>
2019-11-22 07:46:52 +0100

    API with constructor and basic html version after sugestions

--- src/index.js
+++ src/index.js
@@ -3,40 +3,6 @@
  * Code written here will be executed when application launches
  */
 
-class WeatherDataSource {
-    constructor() {
-        this.apiKey = '84af29f03ef35bb4dd7f1e9b0ad575e2';
-    }  
-    getweather(city) {
-      
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=pl&appid=' + this.apiKey)  
-        .then(function(resp) { 
-            return resp.json() })
-        .then(function(data) {
-            console.log(data);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
-    }
+import WeatherDataSource from './weatherDataSource';
 
-}
-
-const input = document.getElementById("submit");
-var city = "";
-var Weather = new WeatherDataSource();
-
-input.addEventListener('click', function(e) {
-    e.preventDefault();
-    city = document.querySelector("#city").value;
-    Weather.getweather(city);
-});
-
-
-
-
-
-    
-
-
-
+console.log('...')
---
+++ src/weatherDataSource.js
@@ -0,0 +1,42 @@
+export default class WeatherDataSource {
+    constructor() {
+        this.apiKey = '84af29f03ef35bb4dd7f1e9b0ad575e2';
+    }  
+    getweather(city) { 
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + this.apiKey)  
+            .then(function(resp) { 
+                return resp.json() })
+            .then(function(data) {
+                console.log(data);
+                showWeather(data);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    }
+}
+const input = document.getElementById("submit");
+var city = "";
+var weather = new WeatherDataSource();
+input.addEventListener('click', function(e) {
+    e.preventDefault();
+    city = document.querySelector("#city").value;
+    weather.getweather(city);
+});
+
+//dodatkowa funkcja związana z pokazywaniem informacji na stronie
+function showWeather(data) {
+    var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+    
+    if( description.indexOf('rain') > 0 ) {
+        document.body.className = 'rainy';
+    } else if( description.indexOf('cloud') > 0 ) {
+        document.body.className = 'cloudy';
+    } else if( description.indexOf('sunny') > 0 ) {
+        document.body.className = 'sunny';
+    }
+    
+    document.getElementById('description').innerHTML = description;
+    document.getElementById('temp').innerHTML = celcius + '&deg;';
+}
