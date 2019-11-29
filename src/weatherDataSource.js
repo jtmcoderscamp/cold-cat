08aaa37b79d602ed67bd2cf393bbe8ff517e75b1
Teresa Ziółkowska <terea.m.ziolkowska@gmail.com>
2019-11-29 08:38:59 +0100

    I added PL language and posiible to show data in houres period

--- src/weatherDataSource.js
+++ src/weatherDataSource.js
@@ -3,7 +3,7 @@ export default class WeatherDataSource {
         this.apiKey = '84af29f03ef35bb4dd7f1e9b0ad575e2';
     }  
     getWeather(city) { 
-        const weatherDataPromise = fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + this.apiKey)  
+        const weatherDataPromise = fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=pl&appid=' + this.apiKey)  
             .then(function(resp) { 
                 return resp.json() })
             .then(function(data) {
@@ -14,6 +14,18 @@ export default class WeatherDataSource {
                 console.log(err);
             });
         return weatherDataPromise 
+    }
+    getForecast(city) { 
+        const forecastDataPromise = fetch('https://api.openweathermap.org/data/2.5/forecast/hourly?q=' + city + '&units=metric&lang=pl&appid=' + this.apiKey)  
+            .then(function(resp) { 
+                return resp.json() })
+            .then(function(data) {
+                console.log(data);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        return forecastDataPromise 
     }
 
 }
