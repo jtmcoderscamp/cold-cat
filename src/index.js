61523d731cad765597d103350a6f366c735a8ac5
barbara-droid <57314438+barbara-droid@users.noreply.github.com>
2019-12-01 20:42:39 +0100

    intergate all app together

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
-/**
- * This file initializes the application
- * Code written here will be executed when application launches
- */
+import "./styles.css";
+import Details from "./details";
+import WeatherDataSource from "./weatherDataSource";
 
-import WeatherDataSource from './weatherDataSource';
+class WeatherApp {
+  constructor(container) {
+    this.container = container;
+    this.city = "";
+    this.weatherData = null;
+    this.dataSource = new WeatherDataSource();
+  }
+  init() {
+    this.container.innerHTML = `
+      <div id="search-city">
+        <form>
+          <label>Wybierz miasto</label>			
+          <select id="city" name="city">
+            <option value=""></option>	
+            <option value="Gdansk">Gdańsk</option>
+            <option value="Krakow">Kraków</option>
+            <option value="Lublin">Lublin</option>
+            <option value="Poznan">Poznań</option>
+            <option value="Przemysl">Przemyśl</option>
+            <option value="Sandomierz">Sandomierz</option>
+            <option value="Szczecin">Szczecin</option>
+            <option value="Torun">Toruń</option>
+            <option value="Warsaw">Warszawa</option>
+            <option value="Wroclaw">Wrocław</option>
+          </select>
+          <button id="submit">Sprawdź pogodę i atrakcje</button>
+        </form>
+        <div id="details" />
+      </div>
+    `;
 
-console.log('...')
+    const loadNewWeatherData = async e => {
+      e.preventDefault();
+      this.weatherData = await this.dataSource.getWeather(this.city);
+      this.forecastData = await this.dataSource.getForecast(this.city);
+      this.details.render(this.weatherData, this.forecastData);
+    };
+
+    this.container
+      .querySelector("#city")
+      .addEventListener("change", e => (this.city = e.target.value));
+
+    this.container
+      .querySelector("#submit")
+      .addEventListener("click", loadNewWeatherData);
+
+    this.details = new Details(this.container.querySelector("#details"));
+  }
+}
+
+const app = new WeatherApp(document.querySelector("body"));
+app.init();
--- src/weatherDataSource.js
+++ src/weatherDataSource.js
@@ -1,39 +1,45 @@
-export default class WeatherDataSource {
-    constructor() {
-        this.apiKey = '84af29f03ef35bb4dd7f1e9b0ad575e2';
-    }  
-    getWeather(city) { 
-        const weatherDataPromise = fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=pl&appid=' + this.apiKey)  
-            .then(function(resp) { 
-                return resp.json() })
-            .then(function(data) {
-                //console.log(data);
-                return new WeatherData(data);
-            })
-            .catch(function(err) {
-                //console.log(err);
-            });
-        return weatherDataPromise 
-    }
-    getForecast(city) { 
-        const forecastDataPromise = fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&lang=pl&appid=' + this.apiKey)  
-            .then(function(resp) { 
-                return resp.json() })
-            .then(function(data) {
-                //console.log(data);
-            })
-            .catch(function(err) {
-                //console.log(err);
-            });
-        return forecastDataPromise 
-    }
+import WeatherData from "./weatherData";
 
+export default class WeatherDataSource {
+  constructor() {
+    this.apiKey = "84af29f03ef35bb4dd7f1e9b0ad575e2";
+  }
+  getWeather(city) {
+    const weatherDataPromise = fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&lang=pl&appid=" +
+        this.apiKey
+    )
+      .then(function(resp) {
+        return resp.json();
+      })
+      .then(function(data) {
+        //console.log(data);
+        return new WeatherData(data);
+      })
+      .catch(function(err) {
+        //console.log(err);
+      });
+    return weatherDataPromise;
+  }
+  getForecast(city) {
+    const forecastDataPromise = fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&units=metric&lang=pl&appid=" +
+        this.apiKey
+    )
+      .then(function(resp) {
+        return resp.json();
+      })
+      .then(function(data) {
+        return data.list ? data.list.map(data => new WeatherData(data)) : [];
+        //console.log(data);
+      })
+      .catch(function(err) {
+        //console.log(err);
+      });
+    return forecastDataPromise;
+  }
 }
-const input = document.getElementById("submit");
-var city = "";
-var weather = new WeatherDataSource();
-input.addEventListener('click', function(e) {
-    e.preventDefault();
-    city = document.querySelector("#city").value;
-    weather.getWeather(city);
-});
